58cd45a6e34e37656d70ee0ec971fc313adb4638
catelynne donnell <110991600+catelynne@users.noreply.github.com>
2024-04-23 08:38:17 -0500

    bubu

---
+++ includes/header.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="./style.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    
+<header> 
+    <nav> 
+    <nav class="navbar bg-dark border-bottom border-body navbar-expand-lg" data-bs-theme="dark">
+   
+   <div class="container-fluid">
+     <a class="navbar-brand" href="home.html">The Football Factory</a>
+     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar-toggler-icon"></span>
+     </button>
+     <div class="collapse navbar-collapse" id="navbarNav">
+       <ul class="navbar-nav">
+         <li class="nav-item">
+           <a class="nav-link active" aria-current="page" href="home.html">Home</a>
+         </li>
+         <li class="nav-item">
+           <a class="nav-link active" href="features.html">Features</a>
+         </li>
+         <li class="nav-item">
+           <a class="nav-link active" href="checkout.html">Checkout</a>
+         </li>
+         <li>
+            <a href="checkout.html"> 
+             <i class="bi bi-bag-heart">  
+             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-heart" viewBox="0 0 16 16">
+               <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0M14 14V5H2v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1M8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132"/>
+             </svg>
+           </i>
+         </a>
+         </li>
+         </ul>
+     </div>
+   </div>
+ </nav>
+        <div>
+            <form action="includes/login.inc.php" method="post">
+                <input type="text" name="mailuid" placeholder="Username/E-mail...">
+                <input type="password" name="pwd" placeholder="Password...">
+                <button type="submit" name="login-submit">Login</button>
+            </form>
+            <a href="signup.php">Signup</a>
+            <form action="includes/logout.inc.php" method="post">
+                <button type="submit" name="logout-submit">Logout</button>
+            </form>
+        </div>
+    </nav>
+</header>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,6 +1,22 @@
-<?php 
-    $sql = "INSERT INTO users ( user_first, user_last, user_email, user_uid, user_pwd)
-    VALUES ('Daniel', 'Nielsen', 'usemmtuts@gmail.com', 'Admin', 'test123');"
-    mysqli_query($conn, $sql);
-    
-    header("Location: ../index.php?signup=success");
+<?php
+ require "header.php";
+?>
+
+    <main> 
+        <div class="wrapper-main">
+            <section class="section-default">
+          <h1>Signup</h1>
+          <form action="includes/signup.inc.php" method="post">
+            <input type="text" name="uid" placeholder="Username">
+            <input type="text" name="mail" placeholder="E-mail">
+            <input type="password" name="pwd" placeholder="Password">
+            <input type="password" name="pwd-repeat" placeholder="Repeat password">
+            <button type="submit" name="signup-submit">Signup</button>
+          </form>
+        </div>
+    </main>
+
+
+<?php
+ require "footer.php";
+?>
